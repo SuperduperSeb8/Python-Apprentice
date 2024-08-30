3858b74753653b9ebdc0f0af055d9e7418f0852c
SuperduperSeb8 <sebastianh@saintjamesacademy.com>
2024-08-30 19:11:45 +0000

    First day with tina the turtle :)

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -14,10 +14,35 @@ turtle.setup (width=600, height=600)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-# Use tina.forward() and tina.left() to draw a triangle
+tina.forward(90) 
+tina.forward(90)
+tina.left(90)
 # Make each side of the triangle a different color with 
-# tina.pencolor()
-
-... # Your code here
-
+tina.pencolor("red")
+tina.pendown
+tina.circle(100)
+tina.isvisible
+tina.forward(90)
+tina.circle(150)
+tina.shapesize(10)
+tina.left(90)
+tina.forward(400)
+tina.begin_fill()
+tina.circle(90)
+tina.end_fill()
+tina.right(180)
+tina.forward(300)
+tina.begin_fill()
+tina.circle(170)
+tina.end_fill()
+tina.begin_fill()
+tina.circle(3000)
+tina.end_fill()
+tina.right(90)
+tina.forward(10)
+tina.begin_fill()
+tina.circle(2000)
+tina.end_fill()
+tina.forward(100)
+tina.right(180)
 turtle.exitonclick()                    # Close the window when we click on it
