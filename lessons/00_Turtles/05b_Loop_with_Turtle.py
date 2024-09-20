ddc4e9e02f350322b8e834e27042c3c434551469
SuperduperSeb8 <sebastianh@saintjamesacademy.com>
2024-09-20 19:14:24 +0000

    Coding day sigma sigma on the wall Sebbbbbb

--- lessons/00_Turtles/05b_Loop_with_Turtle.py
+++ lessons/00_Turtles/05b_Loop_with_Turtle.py
@@ -15,7 +15,8 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
-
+tina.color('yellow')
+tina.begin_fill()
 tina.forward(150)                       # Move tina forward by the forward distance
 tina.left(90)                        # Turn tina left by the left turn
 
@@ -73,6 +74,64 @@ tina.forward(60)
 tina.right(90)
 tina.forward(10)
 tina.left(90)
+
+tina.forward(20)
+tina.right(90)
+tina.forward(80)
+tina.right(90)
+tina.forward(20)
+tina.left(90)
+tina.forward(10)
+tina.right(90)
+tina.forward(60)
+tina.left(90)
+tina.forward(20)
+tina.right(90)
+tina.forward(10)
+tina.right(90)
+tina.forward(20)
+
+tina.right(90)
+tina.forward(70)
+tina.right(90)
+tina.end_fill()
+tina.penup()
+tina.goto(90,20)
+tina.color('brown')
+
+tina.forward(40)
+tina.begin_fill()
+tina.circle(15)
+tina.goto(50,40)
+tina.circle(15)
+tina.goto(10,84)
+tina.circle(15)
+tina.goto(80,13)
+tina.circle(15)
+tina.goto(30,30)
+tina.goto(53,67)
+tina.circle(15)
+tina.goto(71,34)
+tina.circle(15)
+tina.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
